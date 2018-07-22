dd91a4890e4335a3f5437f0af5e0e9aeaafd7304
vasocaity <lazy.yoci11@gmail.com>
2018-07-22 15:22:57 +0700

    edit storyboard

---
+++ TodoApp/TodoItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoItemTableViewCell.swift
+//  TodoApp
+//
+//  Created by Varaphon Maensiri on 22/7/2561 BE.
+//  Copyright © 2561 Varaphon Maensiri. All rights reserved.
+//
+
+import UIKit
+
+class TodoItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
